00b961109b9731d67da8a17f168a8d0971d6e65a
moheck <mcheck96@gmail.com>
2017-04-03 16:44:02 -0700

    first commit

--- app.js
+++ app.js
@@ -48,35 +48,9 @@ intents.matches('TopHeadlines',
         //use builder's EntityRecognizer to findEntity(args.entities, EntityType)
         var source = builder.EntityRecognizer.findEntity(args.entities, 'NewsSource');
         var sourceName = source.entity;
-        if(sourceName === 'buzzfeed'){
-            request('https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=5dc4762d4bb74e37902a33675ac897e3', function(error, response, body){
-                if(!error && response.statusCode == 200){
-                    var body = JSON.parse(body);
-                    // console.log(body);
-                    for(var i=0; i<body.articles.length; i++){
-                        articleTitle = body.articles[i].title;
-                        url = body.articles[i].url;
-                        session.send("Title: %s \n\n URL: %s", articleTitle, url);
-                    }
-                }          
-            }); 
-        }
-        else if(sourceName === 'reddit'){
-            request('https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=top&apiKey=5dc4762d4bb74e37902a33675ac897e3', function(error, response, body){
-                if(!error && response.statusCode == 200){
-                    var body = JSON.parse(body);
-                    // console.log(body);
-                    for(var i=0; i<body.articles.length; i++){
-                        articleTitle = body.articles[i].title;
-                        url = body.articles[i].url;
-                        session.send("Title: %s \n\n URL: %s", articleTitle, url);
-                    }
-                }          
-            }); 
-        }
-        else{
-            session.send("Sorry, I do not recognize that news source");
-        }
+        console.log("News source: " +sourceName);
+        var type = "top";
+        makeRequest(session, sourceName, type);
     }
 );
 
@@ -85,39 +59,100 @@ intents.matches('LatestHeadlines',
     function(session, args){
         //use builder's EntityRecognizer to findEntity(args.entities, EntityType)
         var source = builder.EntityRecognizer.findEntity(args.entities, 'NewsSource');
+        console.log(source);
         var sourceName = source.entity;
-        
-        if(sourceName === 'buzzfeed'){
-            request('https://newsapi.org/v1/articles?source=buzzfeed&sortBy=latest&apiKey=5dc4762d4bb74e37902a33675ac897e3', function(error, response, body){
-                if(!error && response.statusCode == 200){
-                    var body = JSON.parse(body);
-                    // console.log(body);
-                    for(var i=0; i<body.articles.length; i++){
-                        articleTitle = body.articles[i].title;
-                        url = body.articles[i].url;
-                        session.send("Title: %s \n\n URL: %s", articleTitle, url);
-                    }
-                }          
-            }); 
-        }
-        else if(sourceName === 'reddit'){
-            request(' https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=latest&apiKey=5dc4762d4bb74e37902a33675ac897e3', function(error, response, body){
-                if(!error && response.statusCode == 200){
-                    var body = JSON.parse(body);
-                    // console.log(body);
-                    for(var i=0; i<body.articles.length; i++){
-                        articleTitle = body.articles[i].title;
-                        url = body.articles[i].url;
-                        session.send("Title: %s \n\n URL: %s", articleTitle, url);
-                    }
-                }          
-            }); 
-        }
-        else{
-            session.send("Sorry, I do not recognize that news source");
-        }
+        console.log("News source: " + sourceName);
+        var type = "latest";
+        makeRequest(session, sourceName, type);
     }
 );
+
+function makeRequest(session, sourceName, type){
+    if(sourceName.includes('reddit')){
+        sourceName = 'reddit-r-all';
+    }
+    else if(sourceName.includes('aljazeera')){
+        sourceName = 'al-jazeera-english';
+    }
+    else if(sourceName.includes('abc')){
+        sourceName = 'abc-news-au';
+    }
+    else if(sourceName.includes('bbc')){
+        sourceName = 'bbc-news';
+    }
+    else if(sourceName.includes('bloomberg')){
+        sourceName = 'bloomberg';
+    }
+    else if(sourceName.includes('buzzfeed')){
+        sourceName = 'buzzfeed';
+    }
+    else if(sourceName.includes('cnn')){
+        sourceName = 'cnn';
+    }
+    else if(sourceName.includes('espn')){
+        sourceName = 'espn';
+    }
+    else if(sourceName.includes('fortune')){
+        sourceName = 'fortune';
+    }
+    else if(sourceName.includes('reuters')){
+        sourceName = 'reuters';
+    }
+    else if(sourceName.includes('national geographic')){
+        sourceName = 'national-geographic';
+    }
+    else if(sourceName.includes('techcrunch')){
+        sourceName = 'techcrunch';
+    }
+    else if(sourceName.includes("economist")){
+        sourceName = 'the-economist';
+    }
+    else if(sourceName.includes('guardian')){
+        sourceName = 'the-guardian-au';
+    }
+    else if(sourceName.includes('new york times')){
+        sourceName = 'the-new-york-times';
+    }
+    else if(sourceName.includes('huffington post')){
+        sourceName = 'the-huffington-post';
+    }
+    else if(sourceName.includes('wall street journal')){
+        sourceName = 'the-wall-street-journal';
+    }
+    else if(sourceName.includes('washington post')){
+        sourceName = 'the-washington-post';
+    }
+    else if(sourceName.includes('time')){
+        sourceName = 'time';
+    }
+    else if(sourceName.includes('wired')){
+        sourceName = 'wired-de';
+    }
+
+    if(sourceName !== null){
+        var url = 'https://newsapi.org/v1/articles?source=' + sourceName + '&sortBy=' + type + '&apiKey=5dc4762d4bb74e37902a33675ac897e3';
+        request(url, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                var body = JSON.parse(body);
+                console.log(body);
+                for(var i=0; i<body.articles.length; i++){
+                    articleTitle = body.articles[i].title;
+                    url = body.articles[i].url;
+                    session.send("Title: %s \n\n URL: %s", articleTitle, url);
+                    session.endDialog();
+                }
+            }
+            else{
+                session.send("I couldn't find any headlines for that. Try asking for top headlines");
+                session.endDialog();
+            }
+        });
+    }
+    else{
+        session.send("Sorry, I don't recognize that source");
+        session.endDialog();
+    }
+}
 
 // when intent matches regular expression, "Hello", run Greeting Dialog
 intents.matches(/^Hello/i, function(session){
@@ -127,6 +162,7 @@ intents.matches(/^Hello/i, function(session){
 // if intent doesn't match anything, send failure message
 intents.onDefault(function(session){
     session.send("Sorry, I don't know how to handle your request.");
+    session.endDialog();
 });
 
 // Greeting Dialog
